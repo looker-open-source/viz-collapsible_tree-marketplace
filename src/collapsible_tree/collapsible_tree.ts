6bbcd6f0d03cc83d35e2b7ad1243c7602e4f2daf
Cinthia Elizabeth Villalejos Zamora <villalejos@google.com>
2026-06-08 18:00:15 +0000

    fix: done callback handling to mitigate blank tiles update

--- src/collapsible_tree/collapsible_tree.ts
+++ src/collapsible_tree/collapsible_tree.ts
@@ -92,7 +92,7 @@ const vis: CollapsibleTreeVisualization = {
   },
 
   // Render in response to the data or settings changing
-  update(data, element, config, queryResponse) {
+  updateAsync(data, element, config, queryResponse, details, doneRendering) {
     if (
       !handleErrors(this, queryResponse, {
         min_pivots: 0,
@@ -102,228 +102,247 @@ const vis: CollapsibleTreeVisualization = {
         min_measures: 0,
         max_measures: undefined,
       })
-    )
+    ) {
+      doneRendering();
       return;
-
-    let i = 0;
-    const nodeColors = {
-      children:
-        (config && config.color_with_children) ||
-        this.options.color_with_children.default,
-      empty: (config && config.color_empty) || this.options.color_empty.default,
-    };
-    const textSize = 10;
-    const nodeRadius = 4;
-    const duration = 750;
-    const margin = {top: 10, right: 10, bottom: 10, left: 10};
-    const width = element.clientWidth - margin.left - margin.right;
-    const height = element.clientHeight - margin.top - margin.bottom;
-    const linkMap: Map<string, Link[]> = new Map();
-    const nested = burrow(data, queryResponse.fields.dimension_like, linkMap);
-
-    const svg = this.svg!.html('')
-      .attr('width', width + margin.right + margin.left)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    // declares a tree layout and assigns the size
-    const treemap = d3.tree().size([height, width]);
-
-    // Assigns parent, children, height, depth
-    const rootNode: any = d3.hierarchy(nested, d => d.children);
-    rootNode.x0 = height / 2;
-    rootNode.y0 = 0;
-
-    // define some helper functions that close over our local variables
-
-    // Collapse the node and all it's children
-    function collapse(d: any) {
-      if (d.children) {
-        d._children = d.children;
-        d._children.forEach(collapse);
-        d.children = null;
-      }
     }
 
-    // Creates a curved (diagonal) path from parent to the child nodes
-    function diagonal(s: any, d: any) {
-      const path = `
+    try {
+      let i = 0;
+      const nodeColors = {
+        children:
+          (config && config.color_with_children) ||
+          this.options.color_with_children.default,
+        empty:
+          (config && config.color_empty) || this.options.color_empty.default,
+      };
+      const textSize = 10;
+      const nodeRadius = 4;
+      const duration = 750;
+      const margin = {top: 10, right: 10, bottom: 10, left: 10};
+      const width = element.clientWidth - margin.left - margin.right;
+      const height = element.clientHeight - margin.top - margin.bottom;
+      const linkMap: Map<string, Link[]> = new Map();
+      const nested = burrow(data, queryResponse.fields.dimension_like, linkMap);
+
+      const svg = this.svg!.html('')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      // declares a tree layout and assigns the size
+      const treemap = d3.tree().size([height, width]);
+
+      // Assigns parent, children, height, depth
+      const rootNode: any = d3.hierarchy(nested, d => d.children);
+      rootNode.x0 = height / 2;
+      rootNode.y0 = 0;
+
+      // define some helper functions that close over our local variables
+
+      // Collapse the node and all it's children
+      const collapse = (d: any) => {
+        if (d.children) {
+          d._children = d.children;
+          d._children.forEach(collapse);
+          d.children = null;
+        }
+      };
+
+      // Creates a curved (diagonal) path from parent to the child nodes
+      const diagonal = (s: any, d: any) => {
+        const path = `
         M ${s.y} ${s.x}
         C ${(s.y + d.y) / 2} ${s.x},
           ${(s.y + d.y) / 2} ${d.x},
           ${d.y} ${d.x}
       `.trim();
 
-      return path;
-    }
+        return path;
+      };
 
-    // Toggle children on click.
-    function click(d: any) {
-      if (d.children) {
-        d._children = d.children;
-        d.children = null;
-      } else {
-        d.children = d._children;
-        d._children = null;
-      }
-      update(d);
-    }
+      // Toggle children on click.
+      const click = (d: any) => {
+        if (d.children) {
+          d._children = d.children;
+          d.children = null;
+        } else {
+          d.children = d._children;
+          d._children = null;
+        }
+        updateTree(d);
+      };
 
-    // Update the display for a given node
-    function update(source: any) {
-      // Assigns the x and y position for the nodes
-      const treeData = treemap(rootNode);
+      // Update the display for a given node
+      const updateTree = (source: any) => {
+        // Assigns the x and y position for the nodes
+        const treeData = treemap(rootNode);
 
-      // Compute the new tree layout.
-      const nodes = treeData.descendants();
-      const links = treeData.descendants().slice(1);
+        // Compute the new tree layout.
+        const nodes = treeData.descendants();
+        const links = treeData.descendants().slice(1);
 
-      // Normalize for fixed-depth.
-      nodes.forEach(d => {
-        d.y = d.depth * 180;
-      });
-
-      // ****************** Nodes section ***************************
-
-      // Update the nodes...
-      const node = svg
-        .selectAll('g.node')
-        .data(nodes, (d: any) => d.id || (d.id = ++i));
-
-      // Enter any new modes at the parent's previous position.
-      const nodeEnter = node
-        .enter()
-        .append('g')
-        .attr('class', 'node')
-        .attr('transform', (d: any) => {
-          return 'translate(' + source.y0 + ',' + source.x0 + ')';
+        // Normalize for fixed-depth.
+        nodes.forEach(d => {
+          d.y = d.depth * 180;
         });
 
-      // Add Circle for the nodes
-      nodeEnter
-        .append('circle')
-        .attr('class', 'node')
-        .attr('r', 1e-6)
-        .on('click', click);
+        // ****************** Nodes section ***************************
 
-      // Add labels for the nodes
-      nodeEnter
-        .append('text')
-        .attr('dy', '.35em')
-        .attr('x', (d: any) => {
-          return d.children || d._children ? -textSize : textSize;
-        })
-        .attr('text-anchor', (d: any) => {
-          return d.children || d._children ? 'end' : 'start';
-        })
-        .style('cursor', 'pointer')
-        .style('font-family', "'Open Sans', Helvetica, sans-serif")
-        .style('font-size', textSize + 'px')
-        .html((d: any) => {
-          const ellipsizedText = getEllipsizedText(
-            d.data.name,
-            d.y,
-            d.parent?.y,
-            textSize
-          );
-          return `<title>${d.data.name}</title>${ellipsizedText}`;
-        })
-        .on('click', (d: any) => {
-          LookerCharts.Utils.openDrillMenu({
-            links: linkMap.get(d.data.name),
-            event: event,
+        // Update the nodes...
+        const node = svg
+          .selectAll('g.node')
+          .data(nodes, (d: any) => d.id || (d.id = ++i));
+
+        // Enter any new modes at the parent's previous position.
+        const nodeEnter = node
+          .enter()
+          .append('g')
+          .attr('class', 'node')
+          .attr('transform', (d: any) => {
+            return 'translate(' + source.y0 + ',' + source.x0 + ')';
           });
+
+        // Add Circle for the nodes
+        nodeEnter
+          .append('circle')
+          .attr('class', 'node')
+          .attr('r', 1e-6)
+          .on('click', click);
+
+        // Add labels for the nodes
+        nodeEnter
+          .append('text')
+          .attr('dy', '.35em')
+          .attr('x', (d: any) => {
+            return d.children || d._children ? -textSize : textSize;
+          })
+          .attr('text-anchor', (d: any) => {
+            return d.children || d._children ? 'end' : 'start';
+          })
+          .style('cursor', 'pointer')
+          .style('font-family', "'Open Sans', Helvetica, sans-serif")
+          .style('font-size', textSize + 'px')
+          .html((d: any) => {
+            const ellipsizedText = getEllipsizedText(
+              d.data.name,
+              d.y,
+              d.parent?.y,
+              textSize
+            );
+            return `<title>${d.data.name}</title>${ellipsizedText}`;
+          })
+          .on('click', (d: any) => {
+            LookerCharts.Utils.openDrillMenu({
+              links: linkMap.get(d.data.name),
+              event: event,
+            });
+          });
+
+        // UPDATE
+        const nodeUpdate = nodeEnter.merge(node);
+
+        // Transition to the proper position for the node
+        nodeUpdate
+          .transition()
+          .duration(duration)
+          .attr('transform', (d: any) => {
+            return 'translate(' + d.y + ',' + d.x + ')';
+          });
+
+        // Update the node attributes and style
+        nodeUpdate
+          .select('circle.node')
+          .attr('r', nodeRadius)
+          .style('fill', (d: any) =>
+            d._children ? nodeColors.children : nodeColors.empty
+          )
+          .style('stroke', nodeColors.children)
+          .style('stroke-width', 1.5)
+          .attr('cursor', 'pointer');
+
+        // Remove any exiting nodes
+        const nodeExit = node
+          .exit()
+          .transition()
+          .duration(duration)
+          .attr('transform', (d: any) => {
+            return 'translate(' + source.y + ',' + source.x + ')';
+          })
+          .remove();
+
+        // On exit reduce the node circles size to 0
+        nodeExit.select('circle').attr('r', 1e-6);
+
+        // On exit reduce the opacity of text labels
+        nodeExit.select('text').style('fill-opacity', 1e-6);
+
+        // ****************** links section ***************************
+
+        // Update the links...
+        const link = svg.selectAll('path.link').data(links, (d: any) => d.id);
+
+        // Enter any new links at the parent's previous position.
+        const linkEnter = link
+          .enter()
+          .insert('path', 'g')
+          .attr('class', 'link')
+          .style('fill', 'none')
+          .style('stroke', '#ddd')
+          .style('stroke-width', 1.5)
+          .attr('d', (d: any) => {
+            const o = {x: source.x0, y: source.y0};
+            return diagonal(o, o);
+          });
+
+        // UPDATE
+        const linkUpdate = linkEnter.merge(link);
+
+        // Transition back to the parent element position
+        linkUpdate
+          .transition()
+          .duration(duration)
+          .attr('d', (d: any) => diagonal(d, d.parent));
+
+        // Remove any exiting links
+        link
+          .exit()
+          .transition()
+          .duration(duration)
+          .attr('d', (d: any) => {
+            const o = {x: source.x, y: source.y};
+            return diagonal(o, o);
+          })
+          .remove();
+
+        // Store the old positions for transition.
+        nodes.forEach((d: any) => {
+          d.x0 = d.x;
+          d.y0 = d.y;
         });
+      };
 
-      // UPDATE
-      const nodeUpdate = nodeEnter.merge(node);
+      // Collapse after the second level
+      rootNode.children.forEach(collapse);
 
-      // Transition to the proper position for the node
-      nodeUpdate
-        .transition()
-        .duration(duration)
-        .attr('transform', (d: any) => {
-          return 'translate(' + d.y + ',' + d.x + ')';
+      // Update the root node
+      updateTree(rootNode);
+      doneRendering();
+    } catch (error) {
+      console.error('Collapsible Tree Rendering Error:', error);
+
+      if (this.addError) {
+        this.addError({
+          title: 'Rendering Error',
+          message:
+            'An unexpected error occurred while rendering the collapsible tree.',
         });
+      }
 
-      // Update the node attributes and style
-      nodeUpdate
-        .select('circle.node')
-        .attr('r', nodeRadius)
-        .style('fill', (d: any) =>
-          d._children ? nodeColors.children : nodeColors.empty
-        )
-        .style('stroke', nodeColors.children)
-        .style('stroke-width', 1.5)
-        .attr('cursor', 'pointer');
-
-      // Remove any exiting nodes
-      const nodeExit = node
-        .exit()
-        .transition()
-        .duration(duration)
-        .attr('transform', (d: any) => {
-          return 'translate(' + source.y + ',' + source.x + ')';
-        })
-        .remove();
-
-      // On exit reduce the node circles size to 0
-      nodeExit.select('circle').attr('r', 1e-6);
-
-      // On exit reduce the opacity of text labels
-      nodeExit.select('text').style('fill-opacity', 1e-6);
-
-      // ****************** links section ***************************
-
-      // Update the links...
-      const link = svg.selectAll('path.link').data(links, (d: any) => d.id);
-
-      // Enter any new links at the parent's previous position.
-      const linkEnter = link
-        .enter()
-        .insert('path', 'g')
-        .attr('class', 'link')
-        .style('fill', 'none')
-        .style('stroke', '#ddd')
-        .style('stroke-width', 1.5)
-        .attr('d', (d: any) => {
-          const o = {x: source.x0, y: source.y0};
-          return diagonal(o, o);
-        });
-
-      // UPDATE
-      const linkUpdate = linkEnter.merge(link);
-
-      // Transition back to the parent element position
-      linkUpdate
-        .transition()
-        .duration(duration)
-        .attr('d', (d: any) => diagonal(d, d.parent));
-
-      // Remove any exiting links
-      link
-        .exit()
-        .transition()
-        .duration(duration)
-        .attr('d', (d: any) => {
-          const o = {x: source.x, y: source.y};
-          return diagonal(o, o);
-        })
-        .remove();
-
-      // Store the old positions for transition.
-      nodes.forEach((d: any) => {
-        d.x0 = d.x;
-        d.y0 = d.y;
-      });
+      // Release the spinner even if the visualization crashes
+      doneRendering();
     }
-
-    // Collapse after the second level
-    rootNode.children.forEach(collapse);
-
-    // Update the root node
-    update(rootNode);
   },
 };
 
